b3edfc5d09149d1f0e76f81cd1d0e9234660b805
roy-sandoval <roy@roysandoval.com>
2023-08-16 11:59:06 -0700

    Implement ZeroDev Passkeys

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,49 +3,35 @@ import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import {
-  arbitrum,
-  goerli,
-  mainnet,
-  optimism,
-  polygon,
-  zora,
-} from 'wagmi/chains';
+import { optimismGoerli } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    zora,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
-  ],
-  [publicProvider()]
+export const { chains, publicClient, webSocketPublicClient } = configureChains(
+   [optimismGoerli],
+   [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains,
+   appName: 'ZeroDev Passkeys',
+   projectId: 'YOUR_PROJECT_ID',
+   chains,
 });
 
 const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-  webSocketPublicClient,
+   autoConnect: true,
+   connectors,
+   publicClient,
+   webSocketPublicClient,
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
+   return (
+      <WagmiConfig config={wagmiConfig}>
+         <RainbowKitProvider chains={chains}>
+            <Component {...pageProps} />
+         </RainbowKitProvider>
+      </WagmiConfig>
+   );
 }
 
 export default MyApp;
